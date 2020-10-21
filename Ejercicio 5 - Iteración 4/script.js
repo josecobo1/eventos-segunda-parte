9c5a52a73a1240cb0492e7524a70e3bda0772319
Jose Cobo <Jose@MacBook-Pro-de-Jose.local>
2020-10-22 01:12:55 +0200

    Del 5 -4 falta sustituit el case por el eval

--- Ejercicio 5 - Iteración 4/script.js
+++ Ejercicio 5 - Iteración 4/script.js
@@ -13,7 +13,8 @@ let result = document.getElementById("result");
 // A partir de los numeros introducidos y la operación seleccionada
 // ejecuta el calculo correspondiente y devuelve el resultado
 const calculation = (value_1, value_2, operation) => {
-
+    console.log("Valor1: " + value_1);
+    console.log("Valor2: "+ value_2);
     switch(operation) {
         case "+":
             return  value_1 + value_2;
@@ -32,12 +33,20 @@ const calculation = (value_1, value_2, operation) => {
     return res;
 }
 
+const replaceComa = str => {
+    return str.replace(",", ".");
+}
+
 
 // Esta función revisa que los caracteres introducidos son numeros
 // si no son numeros muestra un aviso y si son numeros pasa los valores
 // y la operación seleccionada a la función reponsable de realizar el calculo
 const validation = () => {
-    isNaN(numberOne.value) || isNaN(numberTwo.value) ? alert("Uno de los valores no es un numero") : result.setAttribute("value", calculation(parseInt(numberOne.value), parseInt(numberTwo.value), document.getElementById("operations").value));
+    let value_1 = replaceComa(numberOne.value);
+    let value_2 = replaceComa(numberTwo.value);
+    console.log("Valor1: " + isNaN(value_1));
+    console.log("Valor2: " + isNaN(value_2));
+    isNaN(value_1) || isNaN(value_2) ? alert("Uno de los valores no es un numero") : result.setAttribute("value", calculation(parseFloat(value_1), parseFloat(value_2), document.getElementById("operations").value));
 }
 
 // EventListener que al clicar en el boton realiza los calculos
